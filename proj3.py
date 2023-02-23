50a7d63bb533d52766064b0362390ddf0f11f294
Seena18 <seena101.sa@gmail.com>
2023-02-22 19:09:44 -0800

    recent

---
+++ proj3.py
@@ -0,0 +1,16 @@
+def main():
+    addresses=[]
+    with open('../addresses.txt') as addresses:
+        for line in addresses:
+            address.append
+
+
+
+
+
+
+
+
+
+if __name__="__main__":
+    main()

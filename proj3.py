42d27c41844f87d61112a0606012060cb3108d74
seena18 <seena101.sa@gmail.com>
2023-02-25 21:50:50 -0800

    recent

--- proj3.py
+++ proj3.py
@@ -1,5 +1,38 @@
+import struct
 PAGE_SIZE = 256
-PHYS_MEM_SIZE = 256
+PHYS_MEM_SIZE = 3
+
+# first in first out
+def fifo(physicalMem,b,openFrame,rows,ptable,pagenum,TLB):
+    
+    physicalMem[openFrame[0]]=b.hex()
+    for i in range(16):
+        if(TLB[i][1]==openFrame[0]):
+            TLB[i][0]=pagenum
+    ptable[pagenum]=openFrame[0]
+    for i in range(len(ptable)):
+        if(ptable[i]==openFrame[0] and i!=pagenum):
+            ptable[i]=-1
+    openFrame[0]=(openFrame[0]+1)%PHYS_MEM_SIZE
+
+# least recently used
+def lru(physicalMem,b,openFrame,rows,ptable,pagenum,TLB,stack):
+    index=0
+    try:
+        index=physicalMem.index(None)
+    except:   
+        index=stack.data
+    physicalMem[index]=b.hex()
+    for i in range(16):
+        if(TLB[i][1]==openFrame[0]):
+            TLB[i][0]=pagenum
+    ptable[pagenum]=openFrame[0]
+    for i in range(len(ptable)):
+        if(ptable[i]==openFrame[0] and i!=pagenum):
+            ptable[i]=-1
+  
+
+
 def extractedbits(no,k,pos): 
     bi = bin(no)
     bi=bi[2:]
@@ -9,9 +42,37 @@ def extractedbits(no,k,pos):
     s=bi[31-pos-k:31-pos]
     return int(s,2)
 
+# find node in list by value, return None if not found
+def findNode(head, data):
+    #if not isinstance(head, Node):
+     #       raise TypeError("findNode 'head' must be a Node")
+    node = head
+    while(node != None):
+        if(node.data == data):
+            return node
+        node = node.next
+    return None
+
+# print current state of list
+def printLRU(head):
+    node = head
+    while(node != None):
+        print(node.data + "\n")
+        node = node.next
+
+
+
+class Node:
+   def __init__(self, data=None):
+      self.data = data
+      self.next = None
+      self.prev = None
+
 
 def main():
     PAGE_SIZE = 256
+    lruhead= None
+    lrutail= None
     adds=[]
     TLB=[]
     ptable=[]
@@ -19,7 +80,7 @@ def main():
     tlbIndex=0
     rows, cols=16,2
     hit=0
-    openFrame=0
+    openFrame=[0]
     totalfaults=0
     totalhits=0
 
@@ -33,7 +94,7 @@ def main():
         ptable.append(-1)
 
         
-    with open('./Program_3/addresses.txt') as addresses:
+    with open('./Program_3/one.txt') as addresses:
         for line in addresses:
             adds.append(int(line))
     for i in adds:
@@ -45,32 +106,83 @@ def main():
             if(TLB[j][0]==pagenum):
                 hit=1
                 totalhits+=1
-                framenumber=tlb[j][1]
+                framenumber=TLB[j][1]
                 break
 
 
     
 
         if(not hit):
+            print("TLB MISS")
             if(ptable[pagenum]==-1):
                 with open("./Program_3/BACKING_STORE.bin", mode='rb') as file:
                     file.seek(pagenum*256)
                     b=file.read(256)
-                    physicalMem[openFrame]=b.hex()
-                    openFrame+=1
-                ptable[pagenum] = openFrame-1
+                    lru(physicalMem,b,openFrame,rows,ptable,pagenum,TLB,lruhead)
+                    print("PAGEFAULT")
+                ptable[pagenum]
                 totalfaults+=1
             framenumber=ptable[pagenum]
+            print(framenumber)
             TLB[tlbIndex][0]=pagenum
             TLB[tlbIndex][1]=framenumber
             tlbIndex=(tlbIndex+1)%rows
         index=(framenumber)
         hexa = physicalMem[index]
+        n= findNode(lruhead,index)
+        if(n):
+            # if node at beginning of list, do nothing
+            if(lruhead.data!=n.data):
+                # middle of list
+                if(n.next!= None and n.prev!=None):
+                    n.prev.next=n.next
+                    n.next.prev=n.prev
+                    n.prev = None
+                    n.next = lruhead
+                    lruhead.prev = n
+                    lruhead = n
+                # end of list
+                else:
+                    # update tail
+                    lrutail = n.prev
+                    n.prev.next=None
+                    n.prev = None
+                    n.next = lruhead
+                    lruhead.prev = n
+                    lruhead = n
+        else:
+            # if index not in list, add it
+            nod=Node()
+            nod.data=index
+            # if empty list
+            if (lruhead==None):
+                nod.prev=None
+                nod.next=None
+                lrutail=nod
+                lruhead=nod
+            # not empty
+            else:
+                nod.prev=None
+                nod.next=lruhead
+                lruhead.prev=nod
+                lruhead=nod
+            
         value=hexa[offset*2:(offset*2)+2]
-        x=int(value,16)
-        
-        print(i,offset,value,x,index,hexa,'\n')
+        bits = bin(int(value, 16))[2:].zfill(8)
 
+        if bits[0] == "1":
+            signed_value = -((int("".join("1" if bit == "0" else "0" for bit in bits), 2) + 1) & 0xff)
+        else:
+            signed_value = int(bits, 2)
+
+        print(i,pagenum,signed_value,index,hexa,'\n')
+    print("Number of Translated Addresses = ",len(adds))
+    print("Page Faults = ",totalfaults)
+    print("Page Fault Rate = ", totalfaults/len(adds))
+    print("TLB Hits = ", totalhits)
+    print("TLB Misses = ",(len(adds))-totalhits)
+    print("TLB Hit Rate = ", totalhits/len(adds))
+    print("size of physmem",PHYS_MEM_SIZE)
 
 
 
